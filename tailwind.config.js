cb9ae7b7e0be09d5bae8066fef737d9414361cd7
yawsamcode <samuelacheampong108@gmail.com>
2024-01-30 00:58:18 +0000

    installed all necessary dependencies, fonts, colors. Ready  to kick start

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        SpaceGrotesk: ["Space Grotesk", "sans-serif;"],
+      },
+      colors: {
+        VeryDark: "#151515",
+        PortGreen: "#4EE1A0",
+        DarkGray: "#242424",
+        LightGray: "#242424",
+        PortWhite: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
